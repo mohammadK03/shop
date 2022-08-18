6f6c861599ce006fd31b269a20f1b1caeb14fd87
unknown <mohammadhoseinkazemi1103@gmail.com>
2022-08-18 12:29:09 +0430

    week #3

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -2,13 +2,22 @@ import { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object({
+    title: yup.string().required(),
+    price: yup.string().required(),
+    description: yup.string().required(),
+    image: yup.string().required(),
+    category: yup.string().required(),
+}).required();
 
 const NewProduct = () => {
-    const [title, setTitle] = useState('');
-    const [price, setPrice] = useState(null);
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState('');
-    const [category, setCategory] = useState('');
+    const { register, handleSubmit, reset, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
     const notify = () => toast('Product added successfully!', {
         position: toast.POSITION.BOTTOM_RIGHT,
@@ -16,18 +25,9 @@ const NewProduct = () => {
 
     const Navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        const product = {
-            title,
-            price,
-            description,
-            image,
-            category
-        }
+    const onSubmit = (data) => {
 
-        axios.post('https://fakestoreapi.com/products', product)
+        axios.post('https://fakestoreapi.com/products', data)
             .then(res => {
                 notify();
                 setTimeout(() => {
@@ -45,27 +45,34 @@ const NewProduct = () => {
             <ToastContainer />
 
             <h3 className="text-center mb-5"> Add your new product </h3>
-            <form className="form" onSubmit={handleSubmit}>
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="mt-3">
                     <label className="form-label">Title:</label>
-                    <input className="form-control" type="text" onChange={(e) => setTitle(e.target.value)}/>
+                    <input className="form-control" type="text" {...register('title', {required: true})} />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Price:</label>
-                    <input className="form-control" type="text" onChange={(e) => setPrice(e.target.value)}/>
+                    <input className="form-control" type="text" {...register('price', {required: true})} />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Description:</label>
-                    <input className="form-control" type="text" onChange={(e) => setDescription(e.target.value)}/>
+                    <input className="form-control" type="text" {...register('description', {required: true})} />
                 </div>
                 <div className="mt-3">
                     <label className="form-label">Image:</label>
-                    <input className="form-control" type="text" onChange={(e) => setImage(e.target.value)}/>
+                    <input className="form-control" type="text" {...register('image', {required: true})} />
                 </div>
                 <div className="my-3">
                     <label className="form-label">Category:</label>
-                    <input className="form-control" type="text" onChange={(e) => setCategory(e.target.value)}/>
+                    <input className="form-control" type="text" {...register('category', {required: true})} />
                 </div>
+
+                <p className="error">{errors.title && 'فیلد نام اجباری است.'}</p>
+                <p className="error">{errors.price && 'فیلد قیمت اجباری است.'}</p>
+                <p className="error">{errors.description && 'فیلد توضیحات اجباری است.'}</p>
+                <p className="error">{errors.image && 'فیلد تصویر اجباری است.'}</p>
+                <p className="error">{errors.category && 'فیلد دسته بندی اجباری است.'}</p>
+
                 <button className="btn btn-outline-primary"
                 type="submit">Add</button>
             </form>
